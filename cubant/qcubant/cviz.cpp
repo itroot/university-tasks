9a87af2f2621604cd88de0550bfabc22ebdd7335
Ivan Tolstosheyev <itroot@itroot-asusnb.(none)>
2009-11-03 02:32:08 +0300

    Now we can open file and read it contents

--- cubant/qcubant/cviz.cpp
+++ cubant/qcubant/cviz.cpp
@@ -1,5 +1,12 @@
 #include "cviz.h"
+#include "reper.h"
+
 #include <QtGui>
+#include <QFile>
+#include <QTextStream>
+
+// FIXME
+#include <iostream>
 
 CViz::CViz() {
     imageLabel = new QLabel;
@@ -19,17 +26,43 @@ CViz::CViz() {
 }
 
 void CViz::createActions() {
-
+    openAct = new QAction(tr("&Open..."), this);
+    openAct->setShortcut(tr("Ctrl+O"));
+    connect(openAct, SIGNAL(triggered()), this, SLOT(open()));
+		
     aboutQtAct = new QAction(tr("About &Qt"), this);
     connect(aboutQtAct, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
 }
 
 void CViz::createMenus() {
 
+		fileMenu = new QMenu(tr("&File"), this);
+    fileMenu->addAction(openAct);
+
     helpMenu = new QMenu(tr("&Help"), this);
     helpMenu->addAction(aboutQtAct);
 
+		menuBar()->addMenu(fileMenu);
     menuBar()->addMenu(helpMenu);
+}
+
+void CViz::open() {
+	QString fileName=QFileDialog::getOpenFileName(this,
+                   tr("Open File"), QDir::currentPath());
+	if (!fileName.isEmpty()) {
+		filecontents.clear();
+		QFile file(fileName);
+		if (file.open(QIODevice::ReadOnly)) {
+			QTextStream textStream(&file);
+			while (!textStream.atEnd()) {
+				std::string result=textStream.readLine().toStdString();
+				if (!result.empty()) {
+					filecontents.push_back(result);
+				}
+				std::cerr << result << std::endl;
+			}
+		}
+	}
 }
 
 
--- cubant/qcubant/cviz.h
+++ cubant/qcubant/cviz.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 
 #include <vector>
+#include <string>
+#include <memory>
 
 QT_BEGIN_NAMESPACE
 class QAction;
@@ -13,6 +15,8 @@ class QScrollArea;
 class QScrollBar;
 QT_END_NAMESPACE
 
+class Reper;
+
 class CViz : public QMainWindow {
     Q_OBJECT
 
@@ -20,17 +24,22 @@ public:
     CViz();
 
 private slots:
-
+    void open();
 private:
     void createActions();
     void createMenus();
 
     QLabel *imageLabel;
 
-
+    QAction *openAct;
     QAction *aboutQtAct;
 
+		QMenu *fileMenu;
     QMenu *helpMenu;
+		
+		std::auto_ptr<Reper> reper;
+		// FIXME this shit
+		std::vector<std::string> filecontents;
 };
 
 #endif
