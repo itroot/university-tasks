3e553bb0ab8b1cfa272d8e0255806f9af72b403d
Ivan Tolstosheyev <tolstosheyev@yandex.ru>
2010-02-23 20:03:56 +0300

    Changed backup.js -- roved that we can pass cubant, not it string.

--- qcubant/backup.js
+++ qcubant/backup.js
@@ -1,8 +1,9 @@
 (function() {
+System.setPenWidth(1);
 System.setReperSize(3);
 var cubant=createCubant("/1,1,2/");
 System.print(cubant.dimension());
-System.drawCubantFromString(cubant);
+System.drawCubant(cubant);
 System.setPenWidth(5);
 System.drawCubantFromString("/2,1,1/");
 }
