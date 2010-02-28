d34a7c05ece33b9cdeb3717b9f3d1199133c477c
Ivan Tolstosheyev <tolstosheyev@yandex.ru>
2010-02-28 22:21:49 +0300

    Added drawing of 9-size 3-ways.

--- qcubant/backup.js
+++ qcubant/backup.js
@@ -1,30 +1,58 @@
 (function() {
   System.setPenWidth(1);
-  System.setReperSize(5);
+  System.setReperSize(9);
   System.setImageSize(900,800);
-  System.setCubantStart(400, 580)
+  System.setCubantStart(400, 580);
+  //System.setReper2DVector(0,200,-70);
+  System.setColor(150,150,150);
 
-  System.setReper2DVector(0,-100,-50);
-  System.setReper2DVector(1,-40,-50);
-  System.setReper2DVector(2,0,-50);
-  System.setReper2DVector(3,40,-50);
-  System.setReper2DVector(4,100,-50);
+var a1=[
+"/2,2,2,0,0,0,0,0,0/",
+"/2,2,1,0,0,0,0,0,2/",
+"/2,1,1,0,0,0,0,2,2/",
+"/1,1,1,0,0,0,2,2,2/",
+"/1,1,1,0,0,2,2,2,1/",
+"/1,1,1,0,2,2,2,1,1/",
+"/1,1,1,2,2,2,1,1,1/",
+];
 
-  var cubant=createCubant("/2,2,2,2,2/");
-  System.print(cubant.dimension());
-  System.drawCubant(cubant);
-  System.setPenWidth(5);
-  System.setColor(255,255,0);
-  System.drawCubantFromString("/2,1,1/");
-  var facets=cubantFacet(cubant);
+var a2=[
+"/0,0,0,2,2,2,0,0,0/",
+"/0,0,2,2,2,1,0,0,0/",
+"/0,2,2,2,1,1,0,0,0/",
+"/2,2,2,1,1,1,0,0,0/",
+"/2,2,1,1,1,1,0,0,2/",
+"/2,1,1,1,1,1,0,2,2/",
+"/1,1,1,1,1,1,2,2,2/"
+];
 
+var a3=[
+
+"/0,0,0,0,0,0,2,2,2/",
+"/0,0,0,0,0,2,2,2,1/",
+"/0,0,0,0,2,2,2,1,1/",
+"/0,0,0,2,2,2,1,1,1/",
+"/0,0,2,2,2,1,1,1,1/",
+"/0,2,2,2,1,1,1,1,1/",
+"/2,2,2,1,1,1,1,1,1/"
+]
+  var whole=createCubant("/2,2,2,2,2,2,2,2,2/");
+  System.setPenWidth(1);
+  System.drawCubant(whole);
   System.setColor(0,0,0);
-  var width=1;
-  for(facet in facets) {
-    System.print(facets[facet].toString())
-    System.drawCubant(facets[facet]);
-    ++width;
-    System.setPenWidth(width%4);
+  //System.setColor(255,0,0);
+  System.setPenWidth(2);
+  for (e in a1) {
+    System.drawCubantFromString(a1[e]);
+  }
+
+  //System.setColor(0,255,0);
+  for (e in a2) {
+    System.drawCubantFromString(a2[e]);
+  }
+ //System.setColor(0,0,255);
+  for (e in a3) {
+    System.drawCubantFromString(a3[e]);
   }
 }
 )()
