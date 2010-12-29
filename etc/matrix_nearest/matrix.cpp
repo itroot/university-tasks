d34917c4f47277093fd646c9902314ede5c3de8a
Ivan Tolstosheyev <itroot@yandex-team.ru>
2010-12-29 13:58:46 +0300

    Add tmp matrix cpp impl

---
+++ etc/matrix_nearest/matrix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// 1 - +
+// 0 - 0
+// -1 - -
+
+
+typedef std::vector<std::string> PointsList;
+
+
+
+PointsList generateFacets(unsigned int dimension) {
+    
+}
+
+
+int print_matrix(unsigned int dimension) {
+    
+}
+
+int main() {
+    print_matrix(5);
+}
+
